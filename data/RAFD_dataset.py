3b12f59cbf360ae970971d4e8945dca55d2fbd2d
DiffuseMorph <94506937+DiffuseMorph@users.noreply.github.com>
2021-11-17 12:55:34 +0900

    Add files via upload

---
+++ data/RAFD_dataset.py
@@ -0,0 +1,34 @@
+from torch.utils.data import Dataset
+import data.util as Util
+import os
+import numpy as np
+from skimage import io
+
+class RAFDDataset(Dataset):
+    def __init__(self, dataroot, split='test'):
+        self.split = split
+        self.imageNum = []
+
+        self.datapath = os.path.join(dataroot, split)
+        dataFiles = sorted(os.listdir(self.datapath))
+
+        self.imageNum.append([dataFiles[0], dataFiles[1]])
+        self.data_len = len(self.imageNum)
+
+    def __len__(self):
+        return self.data_len
+
+    def __getitem__(self, index):
+        fileInfo = self.imageNum[index]
+        dataX, dataY = fileInfo[0], fileInfo[1]
+        dataXPath = os.path.join(self.datapath, dataX)
+        dataYPath = os.path.join(self.datapath, dataY)
+        data = io.imread(dataXPath, as_gray=True).astype(float)[:, :, np.newaxis]
+        label = io.imread(dataYPath, as_gray=True).astype(float)[:, :, np.newaxis]
+
+        dataX_RGB = io.imread(dataXPath).astype(float)
+        dataY_RGB = io.imread(dataYPath).astype(float)
+
+        [data, label] = Util.transform_augment([data, label], split=self.split, min_max=(-1, 1))
+
+        return {'M': data, 'F': label, 'MC': dataX_RGB, 'FC': dataY_RGB, 'P':fileInfo, 'Index': index}
---
+++ data/__init__.py
@@ -0,0 +1,32 @@
+'''create dataset and dataloader'''
+import logging
+import torch.utils.data
+
+
+def create_dataloader(dataset, dataset_opt, phase):
+    '''create dataloader '''
+    if phase == 'train':
+        return torch.utils.data.DataLoader(
+            dataset,
+            batch_size=dataset_opt['batch_size'],
+            shuffle=dataset_opt['use_shuffle'],
+            num_workers=dataset_opt['num_workers'],
+            pin_memory=True)
+    elif phase == 'test':
+        return torch.utils.data.DataLoader(
+            dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    else:
+        raise NotImplementedError(
+            'Dataloader [{:s}] is not found.'.format(phase))
+
+def create_dataset_face(dataset_opt, phase):
+    '''create dataset'''
+    from data.RAFD_dataset import RAFDDataset as D
+    dataset = D(dataroot=dataset_opt['dataroot'],
+                split=phase
+                )
+    logger = logging.getLogger('base')
+    logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
+                                                           dataset_opt['name']))
+    return dataset
+
